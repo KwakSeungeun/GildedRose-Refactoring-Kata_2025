3b4706a3f9b5980f74654510009b5837cf69b7be
kanto <kanto@lgcns.com>
2025-02-19 13:29:47 +0900

    refactoring with IDE

--- YongHyeok/Java/src/main/java/com/gildedrose/Item.java
+++ YongHyeok/Java/src/main/java/com/gildedrose/Item.java
@@ -14,8 +14,64 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    void updateItem() {
+        boolean isAgedBrie = name.equals("Aged Brie");
+        boolean isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert");
+
+        if (isAgedBrie) {
+            if (quality < 50) {
+                quality = quality + 1;
+
+            }
+
+            sellIn = sellIn - 1;
+
+            if (sellIn < 0) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        } else {
+            if (isBackstagePass) {
+                if (quality < 50) {
+                    quality = quality + 1;
+
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+            } else {
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
