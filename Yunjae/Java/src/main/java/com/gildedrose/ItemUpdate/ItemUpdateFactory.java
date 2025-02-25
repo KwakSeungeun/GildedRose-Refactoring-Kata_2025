d023c5f66fa519b0ad0a176a423e5c6698feaf7d
juj7347 <juj7347@naver.com>
2025-02-25 22:41:24 +0900

    itemFactory if문에서 map 방식으로 변경

--- Yunjae/Java/src/main/java/com/gildedrose/ItemUpdate/ItemUpdateFactory.java
+++ Yunjae/Java/src/main/java/com/gildedrose/ItemUpdate/ItemUpdateFactory.java
@@ -1,33 +1,20 @@
 package com.gildedrose.ItemUpdate;
 
 import com.gildedrose.*;
+import java.util.HashMap;
+import java.util.Map;
 
-//ItemUpdateFactory는 싱글톤 패턴 적용해도 괜찮을듯
 public class ItemUpdateFactory {
+    private static final Map<String, ItemUpdate> strategies = new HashMap<>();
+
+    static {
+        strategies.put("Aged Brie", new AgedBrieUpdate());
+        strategies.put("Backstage passes to a TAFKAL80ETC concert", new BackStageUpdate());
+        strategies.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdate());
+    }
 
     public static ItemUpdate getItemUpdate(Item item) {
-        switch (item.name) {
-            case "Aged Brie": return new AgedBrieUpdate();
-            case "Backstage passes to a TAFKAL80ETC concert": return new BackStageUpdate();
-            case "Sulfuras, Hand of Ragnaros": return new SulfurasUpdate();
-            default: return new DefaultUpdate();
-
-        }
+        return strategies.getOrDefault(item.name, new DefaultUpdate());
     }
 }
-
-
-//class ItemStrategyFactory {
-//    private static final Map<String, ItemStrategy> strategies = new HashMap<>();
-//
-//    static {
-//        strategies.put("Aged Brie", new AgedBrieStrategy());
-//        strategies.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassStrategy());
-//        strategies.put("Sulfuras, Hand of Ragnaros", new SulfurasStrategy());
-//    }
-//
-//    public static ItemStrategy getStrategy(Item item) {
-//        return strategies.getOrDefault(item.name, new DefaultItemStrategy());
-//    }
-//}
 
